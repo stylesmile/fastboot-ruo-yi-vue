59a289f272db87188f6fc113558cd182b7aa6ec3
Stylesmile <3239866994@qq.com>
2024-02-23 23:10:59 +0800

    RuoFastbootYiApplication.java

---
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoFastbootYiApplication.java
@@ -0,0 +1,27 @@
+package com.ruoyi;
+
+import io.github.stylesmile.app.App;
+
+/**
+ * 启动程序
+ * 
+ * @author ruoyi
+ */
+public class RuoFastbootYiApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        App.start(RuoFastbootYiApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
