c0a5a7541082e902c0ba6a2a6d16a9c4124f94ed
Stylesmile <3239866994@qq.com>
2024-03-26 14:48:05 +0800

    个人信息 业务处理

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
@@ -1,15 +1,5 @@
 package com.ruoyi.web.controller.system;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.config.RuoYiConfig;
 import com.ruoyi.common.core.controller.BaseController;
@@ -17,34 +7,39 @@ import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.domain.entity.SysUser;
 import com.ruoyi.common.core.domain.model.LoginUser;
 import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.common.utils.SecurityUtils;
+import com.ruoyi.common.utils.MD5Util;
 import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.common.utils.file.FileUploadUtils;
 import com.ruoyi.common.utils.file.MimeTypeUtils;
 import com.ruoyi.framework.web.service.TokenService;
 import com.ruoyi.system.service.ISysUserService;
+import io.github.stylesmile.annotation.AutoWired;
+import io.github.stylesmile.annotation.RequestMapping;
+import io.github.stylesmile.annotation.RequestParam;
+import io.github.stylesmile.file.UploadedFile;
+import io.github.stylesmile.request.RequestMethod;
+import org.springframework.stereotype.Controller;
 
 /**
  * 个人信息 业务处理
- * 
+ *
  * @author ruoyi
  */
 @Controller
-@RequestMapping("/system/user/profile")
-public class ControllerSysProfileController extends BaseController
-{
-    @Autowired
+//@RequestMapping("/system/user/profile")
+public class SysProfileController extends BaseController {
+    @AutoWired
     private ISysUserService userService;
 
-    @Autowired
+    @AutoWired
     private TokenService tokenService;
 
     /**
      * 个人信息
      */
-    @GetMapping
-    public AjaxResult profile()
-    {
+//    @GetMapping
+    @RequestMapping("get")
+    public AjaxResult profile() {
         LoginUser loginUser = getLoginUser();
         SysUser user = loginUser.getUser();
         AjaxResult ajax = AjaxResult.success(user);
@@ -57,27 +52,25 @@ public class ControllerSysProfileController extends BaseController
      * 修改用户
      */
     @Log(title = "个人信息", businessType = BusinessType.UPDATE)
-    @PutMapping
-    public AjaxResult updateProfile(@RequestBody SysUser user)
-    {
+//    @PutMapping
+    @RequestMapping("edit")
+//    public AjaxResult updateProfile(@RequestBody SysUser user)
+    public AjaxResult updateProfile(SysUser user) {
         LoginUser loginUser = getLoginUser();
         SysUser currentUser = loginUser.getUser();
         currentUser.setNickName(user.getNickName());
         currentUser.setEmail(user.getEmail());
         currentUser.setPhonenumber(user.getPhonenumber());
         currentUser.setSex(user.getSex());
-        if (StringUtils.isNotEmpty(user.getPhonenumber()) && !userService.checkPhoneUnique(currentUser))
-        {
+        if (StringUtils.isNotEmpty(user.getPhonenumber()) && !userService.checkPhoneUnique(currentUser)) {
             return error("修改用户'" + loginUser.getUsername() + "'失败，手机号码已存在");
         }
-        if (StringUtils.isNotEmpty(user.getEmail()) && !userService.checkEmailUnique(currentUser))
-        {
+        if (StringUtils.isNotEmpty(user.getEmail()) && !userService.checkEmailUnique(currentUser)) {
             return error("修改用户'" + loginUser.getUsername() + "'失败，邮箱账号已存在");
         }
-        if (userService.updateUserProfile(currentUser) > 0)
-        {
+        if (userService.updateUserProfile(currentUser) > 0) {
             // 更新缓存用户信息
-            tokenService.setLoginUser(loginUser);
+//            tokenService.setLoginUser(loginUser);
             return success();
         }
         return error("修改个人信息异常，请联系管理员");
@@ -87,23 +80,21 @@ public class ControllerSysProfileController extends BaseController
      * 重置密码
      */
     @Log(title = "个人信息", businessType = BusinessType.UPDATE)
-    @PutMapping("/updatePwd")
-    public AjaxResult updatePwd(String oldPassword, String newPassword)
-    {
+    @RequestMapping(value = "/updatePwd", method = RequestMethod.PUT)
+    public AjaxResult updatePwd(String oldPassword, String newPassword) {
         LoginUser loginUser = getLoginUser();
         String userName = loginUser.getUsername();
         String password = loginUser.getPassword();
-        if (!SecurityUtils.matchesPassword(oldPassword, password))
-        {
+//        if (!SecurityUtils.matchesPassword(oldPassword, password))
+        if (password.equals(MD5Util.calculateMD5(oldPassword))) {
             return error("修改密码失败，旧密码错误");
         }
-        if (SecurityUtils.matchesPassword(newPassword, password))
-        {
+        if (password.equals(MD5Util.calculateMD5(oldPassword))) {
             return error("新密码不能与旧密码相同");
         }
-        newPassword = SecurityUtils.encryptPassword(newPassword);
-        if (userService.resetUserPwd(userName, newPassword) > 0)
-        {
+//        newPassword = SecurityUtils.encryptPassword(newPassword);
+        newPassword = MD5Util.calculateMD5(newPassword);
+        if (userService.resetUserPwd(userName, newPassword) > 0) {
             // 更新缓存用户密码
             loginUser.getUser().setPassword(newPassword);
             tokenService.setLoginUser(loginUser);
@@ -116,15 +107,12 @@ public class ControllerSysProfileController extends BaseController
      * 头像上传
      */
     @Log(title = "用户头像", businessType = BusinessType.UPDATE)
-    @PostMapping("/avatar")
-    public AjaxResult avatar(@RequestParam("avatarfile") MultipartFile file) throws Exception
-    {
-        if (!file.isEmpty())
-        {
+    @RequestMapping(value = "/avatar", method = RequestMethod.POST)
+    public AjaxResult avatar(@RequestParam("avatarfile") UploadedFile file) throws Exception {
+        if (file.getContentSize() > 0) {
             LoginUser loginUser = getLoginUser();
             String avatar = FileUploadUtils.upload(RuoYiConfig.getAvatarPath(), file, MimeTypeUtils.IMAGE_EXTENSION);
-            if (userService.updateUserAvatar(loginUser.getUsername(), avatar))
-            {
+            if (userService.updateUserAvatar(loginUser.getUsername(), avatar)) {
                 AjaxResult ajax = AjaxResult.success();
                 ajax.put("imgUrl", avatar);
                 // 更新缓存用户头像
