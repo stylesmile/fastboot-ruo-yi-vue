8955adee42ec5acb1612515483a75b383ce9ee1a
Stylesmile <3239866994@qq.com>
2024-03-26 14:07:48 +0800

    注释未完成代码

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/ServerController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/ServerController.java
@@ -1,28 +1,28 @@
-package com.ruoyi.web.controller.monitor;
-
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.framework.web.domain.Server;
-
-/**
- * 服务器监控
- * 
- * @author ruoyi
- */
-@Controller
-//@RequestMapping("/monitor/server")
-public class ServerController
-{
-    @PreAuthorize("@ss.hasPermi('monitor:server:list')")
-    @GetMapping()
-    public AjaxResult getInfo() throws Exception
-    {
-        Server server = new Server();
-        server.copyTo();
-        return AjaxResult.success(server);
-    }
-}
+//package com.ruoyi.web.controller.monitor;
+//
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import com.ruoyi.common.core.domain.AjaxResult;
+//import com.ruoyi.framework.web.domain.Server;
+//
+///**
+// * 服务器监控
+// *
+// * @author ruoyi
+// */
+//@Controller
+////@RequestMapping("/monitor/server")
+//public class ServerController
+//{
+//    @PreAuthorize("@ss.hasPermi('monitor:server:list')")
+//    @GetMapping()
+//    public AjaxResult getInfo() throws Exception
+//    {
+//        Server server = new Server();
+//        server.copyTo();
+//        return AjaxResult.success(server);
+//    }
+//}
